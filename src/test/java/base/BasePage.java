fea28fb5a22bb817db71caa5159606514855a73c
reachharikk-eng <reachharikk@gmail.com>
2026-05-21 12:17:07 +0530

    Added reusable BasePage utility methods

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,4 +1,64 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+
+    WebDriver driver;
+
+    WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+
+        this.driver = driver;
+
+        wait = new WebDriverWait(driver,
+                Duration.ofSeconds(10));
+    }
+
+    public void clickElement(WebElement element) {
+
+        waitForVisibility(element);
+
+        element.click();
+    }
+
+    public void enterText(WebElement element,
+                          String text) {
+
+        waitForVisibility(element);
+
+        element.clear();
+
+        element.sendKeys(text);
+    }
+
+    public String getElementText(WebElement element) {
+
+        waitForVisibility(element);
+
+        return element.getText();
+    }
+
+    public void selectDropdownByVisibleText(
+            WebElement element,
+            String value) {
+
+        Select select = new Select(element);
+
+        select.selectByVisibleText(value);
+    }
+
+    public void waitForVisibility(
+            WebElement element) {
+
+        wait.until(
+                ExpectedConditions.visibilityOf(element));
+    }
 }
