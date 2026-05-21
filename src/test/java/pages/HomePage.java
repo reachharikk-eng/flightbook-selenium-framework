b9bdd1e2ba3ce80e7c65d3f90c7259891d93e26e
reachharikk-eng <reachharikk@gmail.com>
2026-05-21 13:00:51 +0530

    Implemented first flight search automation test

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,4 +1,60 @@
 package pages;
 
-public class HomePage {
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+
+        super(driver);
+
+        this.driver = driver;
+
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(name = "fromPort")
+    WebElement departureDropdown;
+
+    @FindBy(name = "toPort")
+    WebElement destinationDropdown;
+
+    @FindBy(css = "input[type='submit']")
+    WebElement findFlightsButton;
+
+    public void selectDepartureCity(String city) {
+
+        selectDropdownByVisibleText(
+                departureDropdown,
+                city);
+    }
+
+    public void selectDestinationCity(String city) {
+
+        selectDropdownByVisibleText(
+                destinationDropdown,
+                city);
+    }
+
+    public void clickFindFlights() {
+
+        clickElement(findFlightsButton);
+    }
+
+    public void searchFlight(
+            String departure,
+            String destination) {
+
+        selectDepartureCity(departure);
+
+        selectDestinationCity(destination);
+
+        clickFindFlights();
+    }
 }
--- src/test/java/tests/FlightSearchTest.java
+++ src/test/java/tests/FlightSearchTest.java
@@ -1,4 +1,25 @@
 package tests;
 
-public class FlightSearchTest {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class FlightSearchTest extends BaseTest {
+
+    @Test
+    public void verifyFlightSearch() {
+
+        HomePage homePage = new HomePage(driver);
+
+        homePage.searchFlight(
+                "Boston",
+                "London");
+
+        String currentUrl = driver.getCurrentUrl();
+
+        Assert.assertTrue(
+                currentUrl.contains("reserve"),
+                "Flight search failed");
+    }
 }
